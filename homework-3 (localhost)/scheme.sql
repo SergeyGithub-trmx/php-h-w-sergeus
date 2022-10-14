a6b48be8e0e28c4344a2d189f9d94d4457fe7a4c
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-10-14 20:14:51 +1000

    Update scheme.sql

--- homework-3 (localhost)/scheme.sql
+++ homework-3 (localhost)/scheme.sql
@@ -1,1 +1,24 @@
+-- CREATE TABLE user (
+--     ID int NOT NULL PRIMARY KEY,
+--     login nvarchar(15) NOT NULL
+--     email nvarchar(30),
+--     password_hash nvarchar(16) NOT NULL
+-- )
 
+-- CREATE TABLE task (
+--     ID int NOT NULL PRIMARY KEY,
+--     name nvarchar(255) NOT NULL,
+--     create_task datetime NOT NULL,
+--     update_task datetime NOT NULL,
+--     task_status text NOT NULL
+-- )
+
+-- SELECT * 
+-- FROM task
+-- WHERE task_status = 'ACTIVE' OR 'EXPIRED'
+
+-- CREATE TABLE project (
+--     ID int NOT NULL PRIMARY KEY,
+--     name nvarchar(255) NOT NULL,
+--     create_project datetime NOT NULL
+-- )
