1b7d50126c5a01493a527338b4423db60b170d26
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-10-30 11:51:50 +1000

    Update queries.sql

--- homework-3 (localhost)/queries.sql
+++ homework-3 (localhost)/queries.sql
@@ -1,3 +1,5 @@
+USE `schema`;
+
 INSERT IGNORE INTO `user` (login, email, password_hash) VALUES ('SergHecker', 'trojan.win32.serGEEi@mail.ru', 'As.[#_,LG?6*EC]M^]n-');
 INSERT IGNORE INTO `user` (login, email, password_hash) VALUES ('AUGMENTARO', 'microsoft.myspecacc@outlook.com', ']Rt&7dzl+N^6<5r)OEYV');
 INSERT IGNORE INTO `user` (login, email, password_hash) VALUES ('mylifebelikeooayauu', 'den.500thousands-consolelogs@hotmail.com', '_?&B\:\1.R1TX%&x/_Ue');
