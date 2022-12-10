b4ee9c14f5b1857ca28e453f0edd98e693e92294
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-12-10 20:49:33 +1000

    Update guest-page.php

--- homework-3 (localhost)/guest-page.php
+++ homework-3 (localhost)/guest-page.php
@@ -2,15 +2,9 @@
 
 require_once('functions.php');
 
-$page_content = includeTemplate('guest.php');
-
-$page_header = includeTemplate('layouts/header.php');
-
-$page_layout = includeTemplate('layouts/main-layout.php', [
-    'page_content' => $page_content,
-    'page_header' => $page_header,
-    'css_file' => './css/guest_page-style.css',
+print(includeTemplate('layouts\main-layout.php', [
+    'page_content' => includeTemplate('guest.php'),
+    'page_header' => includeTemplate('layouts\header.php'),
+    'css_file' => 'css\guest_page-style.css',
     'title' => 'Task Manager - Guest page'
-]);
-
-print($page_layout);
+]));
