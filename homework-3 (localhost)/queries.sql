7da7914813f3a19e5629c303fc9f852fec667ceb
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-12-10 20:50:30 +1000

    Update queries.sql

--- homework-3 (localhost)/queries.sql
+++ homework-3 (localhost)/queries.sql
@@ -1,9 +1,20 @@
-USE `schema`;
+INSERT INTO `user` (login, email, password_hash) VALUES
+('SergHecker', 'trojan.win32.serGEEi@mail.ru', 'As.[#_,LG?6*EC]M^]n-'),
+('AUGMENTARO', 'microsoft.myspecacc@outlook.com', ']Rt&7dzl+N^6<5r)OcYV'),
+('mylifebelikeooayauu', 'den.500thousands-consolelogs@hotmail.com', '_?&B\:\1.R1TX%&x/_Ue'),
+('TheGrogy', 'gorbunek@gmail.com', '4$6./w0Gr75Mb61_0-1!'),
+('IgorKLhacker', 'idontknow@inbox.ru', '+]2\?.74Gb&jK0-=27Hs');
 
-INSERT IGNORE INTO `user` (login, email, password_hash) VALUES ('SergHecker', 'trojan.win32.serGEEi@mail.ru', 'As.[#_,LG?6*EC]M^]n-');
-INSERT IGNORE INTO `user` (login, email, password_hash) VALUES ('AUGMENTARO', 'microsoft.myspecacc@outlook.com', ']Rt&7dzl+N^6<5r)OEYV');
-INSERT IGNORE INTO `user` (login, email, password_hash) VALUES ('mylifebelikeooayauu', 'den.500thousands-consolelogs@hotmail.com', '_?&B\:\1.R1TX%&x/_Ue');
+INSERT INTO `project` (name, user_id) VALUES
+('Everyday doings', 1),
+('Hacking with Backdoor', 2),
+('HTML-virus with Exploit', 3),
+('First CTF-Pentesting', 4),
+('Install KALI and msfconsole', 5);
 
-INSERT IGNORE INTO `project` (name) VALUES ('Everyday doings');
-
-INSERT IGNORE INTO `task` (name) VALUES ('Shopping');
+INSERT INTO `task` (name, user_id, project_id) VALUES
+('Shopping', 1, 1),
+('Prank friend with Exploit', 2, 2),
+('Upload my new HTML-virus', 3, 3),
+('Install RMS', 4, 4),
+('Try old ms17-010-eternalblue', 5, 5);
