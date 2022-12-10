8a70a409006883840d52774b8d4d8f5f2b336158
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-12-10 20:49:04 +1000

    Update main-page.php

--- homework-3 (localhost)/main-page.php
+++ homework-3 (localhost)/main-page.php
@@ -2,14 +2,42 @@
 
 require_once('functions.php');
 
-$page_content = includeTemplate('main.php');
+$config = require_once('config\db.php');
+$mysqli = mysqli_connect(...$config);
 
-$page_header = includeTemplate('layouts/header.php');
+if (!$mysqli) {
+    http_response_code(500);
+    exit;
+}
 
-$page_layout = includeTemplate('layouts/main-layout.php', [
+$sql = 'SELECT * FROM `project`';
+$projresult = mysqli_query($mysqli, $sql);
+
+$sql = 'SELECT * FROM `task`';
+$taskresult = mysqli_query($mysqli, $sql);
+
+$projects = [];
+$tasks = [];
+
+while ($project = mysqli_fetch_assoc($projresult)) {
+    $projects[] = $project;
+}
+
+while ($task = mysqli_fetch_assoc($taskresult)) {
+    $tasks[] = $task;
+}
+
+$page_content = includeTemplate('main.php', [
+    'tasks' => $tasks,
+    'projects' => $projects
+]);
+
+$page_header = includeTemplate('layouts\header.php');
+
+$page_layout = includeTemplate('layouts\main-layout.php', [
     'page_content' => $page_content,
     'page_header' => $page_header,
-    'css_file' => './css/main_page-style.css',
+    'css_file' => 'css\main_page-style.css',
     'title' => 'Task Manager - Main page'
 ]);
 
