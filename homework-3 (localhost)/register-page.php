4c961ca89c9a19d0123deaa92fa2e5646968ff75
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-12-10 20:48:41 +1000

    Update register-page.php

--- homework-3 (localhost)/register-page.php
+++ homework-3 (localhost)/register-page.php
@@ -1,15 +1,63 @@
 <?php
 
 require_once('functions.php');
+require_once('db-func.php');
 
-$page_content = includeTemplate('register.php');
+$config = require_once('config\db.php');
+$mysqli = mysqli_connect(...$config);
 
-$page_header = includeTemplate('layouts/header.php');
+if (!$mysqli) {
+    http_response_code(500);
+    exit;
+};
 
-$page_layout = includeTemplate('layouts/main-layout.php', [
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = trim($_POST['login'] ?? '');
+    $email = trim($_POST['email'] ?? '');
+    $password = trim($_POST['password'] ?? '');
+
+    if (mb_strlen($login) === 0) {
+        $errors['login'] = 'Please enter a login.';
+    }
+
+    if (mb_strlen($email) === 0) {
+        $errors['email'] = 'Please enter a valid email address.';
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Please enter a valid email address.';
+    }
+
+    if (mb_strlen($password) === 0) {
+        $errors['password'] = 'Please enter a password!';
+    }
+
+    if (empty($errors)) {
+        if ($user = get_user_by_email($mysqli, $email)) {
+
+            if (password_verify($password, $user['password_hash'])) {
+                $_SESSION['user'] = $user;
+                header('Location: /main-page.php');
+            } else {
+                $errors['password'] = 'Please enter a password!';
+            }
+        } else {
+            $errors['password'] = 'Please enter a password!';
+        }
+    }
+}
+
+$page_content = includeTemplate('register.php', [
+    'errors' => $errors
+]);
+
+$page_header = includeTemplate('layouts\header.php');
+
+$page_layout = includeTemplate('layouts\main-layout.php', [
     'page_content' => $page_content,
     'page_header' => $page_header,
-    'css_file' => './css/login-register_page-style.css',
+    'css_file' => 'css\login-register_page-style.css',
     'title' => 'Task Manager - Register'
 ]);
 
